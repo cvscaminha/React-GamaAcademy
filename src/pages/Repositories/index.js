9b0930032355396a498a751e5ac98912706af072
Cristiano Vieira <cvscaminha@hotmail.com>
2021-07-13 00:03:57 -0300

    Upload Project for GitHub

---
+++ src/pages/Repositories/index.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import * as S from "./styled";
+import { useHistory } from "react-router-dom";
+
+function Repositories() {
+  const history = useHistory();
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    let repositoriesName = localStorage.getItem("repositoriesName");
+    if (repositoriesName != null) {
+      repositoriesName = JSON.parse(repositoriesName);
+      setRepositories(repositoriesName);
+      localStorage.clear();
+    } else {
+      history.push("/");
+    }
+  }, []);
+
+  return (
+    <S.Container>
+      <S.Title>Repositórios</S.Title>
+      <S.list>
+        {repositories.map((repository) => {
+          return <S.ListItem>Repositório: {repository}</S.ListItem>;
+        })}
+      </S.list>
+      <S.LinkHome to="/">Voltar</S.LinkHome>
+    </S.Container>
+  );
+}
+
+export default Repositories;
